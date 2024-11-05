4280f5f2c2759f5c31bcb41a35ec7f2a76cebfb1
ZeinabHamdy <zeinabhamdy1010@gmail.com>
2024-11-05 16:10:39 +0200

    add author, category models -still in progress

--- books/admin.py
+++ books/admin.py
@@ -3,3 +3,5 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.Book)
+admin.site.register(models.Author)
+admin.site.register(models.Category)
--- books/models.py
+++ books/models.py
@@ -4,28 +4,28 @@ from django.core.validators import MinValueValidator
 # Create your models here.
 class Book(models.Model) :
     title = models.CharField(
-        max_length = 150,
+        max_length = 100,
         null = False, # default value 
         blank = False, #default value
-        default = 'no title'
+        default = 'no title',
     )
     price = models.DecimalField(
-        max_digits = 6,
+        max_digits = 8,
         decimal_places = 2,
         null = False,
         blank = False,
         default=0.0,
-        validators=[MinValueValidator(0.00)]
+        validators=[MinValueValidator(0.00)],
     )
     discount = models.DecimalField(
-        max_digits = 6,
+        max_digits = 8,
         decimal_places = 2,
         null = True,
         blank = True,
         validators=[MinValueValidator(0.00)],
     )
     rate = models.DecimalField(
-        max_digits = 6,
+        max_digits = 8,
         decimal_places = 2,
         null = True,
         blank = True,
@@ -36,3 +36,29 @@ class Book(models.Model) :
 
     def __str__(self):
         return self.title
+
+
+
+class Author(models.Model):
+    name = models.CharField(
+        max_length = 100,
+        null = False,
+        blank = False,
+    )
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length = 100,
+        null = False,
+        blank = False,
+    )
+
+
+    def __str__(self):
+        return self.name
